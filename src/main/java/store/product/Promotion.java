277278188a8aadf79357d805f640d07fcd80817c
mete0rfish <sungwon326@naver.com>
2024-11-07 18:19:38 +0900

    fix(promotion): Promotion의 of 메서드 추가
    String을 Date로 변환하기 위한 DateFormat 생성

--- src/main/java/store/product/Promotion.java
+++ src/main/java/store/product/Promotion.java
@@ -1,16 +1,20 @@
 package store.product;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Objects;
 
 import camp.nextstep.edu.missionutils.*;
+import store.product.dto.PromotionReadResponse;
+
+import static store.util.DateFormat.formatter;
 
 public record Promotion(
         String name,
-        Long buyCount,
-        Long getCount,
-        Date start_date,
-        Date end_date
+        int buyCount,
+        int getCount,
+        Date startDate,
+        Date endDate
 ) {
 
     public Promotion findByName(String name) {
@@ -18,5 +22,24 @@ public record Promotion(
             return this;
         }
         return null;
+    }
+
+    public Promotion of(PromotionReadResponse response) {
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            startDate = formatter.parse(response.start_date());
+            endDate = formatter.parse(response.end_date());
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            // TODO 커스텀 예외
+        }
+        return new Promotion(
+                response.name(),
+                response.buy(),
+                response.get(),
+                startDate,
+                endDate
+        );
     }
 }
---
+++ src/main/java/store/util/DateFormat.java
@@ -0,0 +1,7 @@
+package store.util;
+
+import java.text.SimpleDateFormat;
+
+public class DateFormat {
+    public final static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+}
