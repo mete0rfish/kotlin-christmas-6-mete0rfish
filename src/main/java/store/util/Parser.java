72a01c4a5a50efa2e973a24c1cc1e22696dc300b
mete0rfish <sungwon326@naver.com>
2024-11-10 19:35:26 +0900

    fix: 입력의 잘못된 형식 예외처리

--- src/main/java/store/util/Parser.java
+++ src/main/java/store/util/Parser.java
@@ -24,6 +24,8 @@ public class Parser {
             return productMap;
         } catch (NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        } catch (ArrayIndexOutOfBoundsException e){
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
     }
 }
