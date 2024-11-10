578535b5723de3da32c2b5e7fe0ba0bcf30e2e71
mete0rfish <sungwon326@naver.com>
2024-11-10 19:31:16 +0900

    test(product): 예외 메시지 수정

--- src/test/java/store/product/ProductTest.java
+++ src/test/java/store/product/ProductTest.java
@@ -69,6 +69,6 @@ class ProductTest {
         assertThatThrownBy(() -> {
             Parser.parseBuyInput(input);
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 수량 형식이 올바르지 않습니다.");
+                .hasMessage("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
     }
 }
