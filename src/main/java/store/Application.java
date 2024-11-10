ba2a89f4e17706e3270dce1c741650c01cd2a4c1
mete0rfish <sungwon326@naver.com>
2024-11-10 17:18:51 +0900

    feat(main): Controller 로직 수행

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,16 @@
 package store;
 
+import store.product.controller.ProductController;
+import store.product.view.InputView;
+import store.product.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        ProductController controller = new ProductController(inputView, outputView);
+        controller.run();
     }
 }
